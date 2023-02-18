c4083cdfb3677820bb62059278194086dc06c032
Егор <egor@MacBook-Air-Egor-2.local>
2023-02-18 19:13:07 +0300

    Users View

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin
 
-from .serializers import ContactSerializer
-from .models import Contact
+from .serializers import ContactSerializer,UserSerializer
+from .models import Contact,CustomUser
 
 
 
@@ -11,5 +14,38 @@ class ContactViewSet(ModelViewSet):
     serializer_class = ContactSerializer
     permission_classes = (IsAuthenticated,)
     queryset = Contact.objects.all()
+
+
+class GetCurUserDataView(GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return user
+
+    def get(self,request,*args,**kwargs):
+        queryset = self.get_queryset()
+        serializer = self.serializer_class(queryset,*args,*kwargs)
+        return Response(serializer.data)
+
+
+class GetSuckersListView(ListModelMixin,GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        if self.request.user.is_HR:
+            HR_pk = self.request.user.pk
+
+            users = CustomUser.objects.filter(HR_link=HR_pk)
+            return users
+        HR_pk = self.request.user.HR_link
+        users = CustomUser.objects.filter(pk=HR_pk)
+        return users
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+
 
 
