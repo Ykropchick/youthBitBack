60d7adf5ab11ed65f8b38a079b1f50abe7fa2f1f
Kirill <shkolenko02-03-2004@mail.ru>
2023-02-19 06:55:40 +0300

    Correct build file

--- Onboarding/urls.py
+++ Onboarding/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('tests/',include('tests.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
